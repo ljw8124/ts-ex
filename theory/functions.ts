622b63d6d81a0112a6c007570b36731deae79c9a
Joungwoo Lee <ljw8124@gmail.com>
2024-04-30 20:34:13 +0900

    overloading

--- theory/functions.ts
+++ theory/functions.ts
@@ -8,3 +8,49 @@ type Add = (a: number, b: number) => number;
 // Add 의 타입을 알기 때문에 a 와 b 가 number 라는 것을 앎
 const add:Add = (a, b) => a + b;
 
+
+// overloading Example
+type Minus = {
+    (a: number, b: number) : number
+    (a: number, b: string) : number
+}
+
+const minus: Minus = (a, b) => {
+    if(typeof b === "string") return a;
+
+    return a + b;
+}
+
+type Config = {
+    path: string,
+    state: object
+}
+
+type Push = {
+    (path: string) : void
+    (config: Config): void
+}
+
+const push:Push = (config) => {
+    if(typeof config === "string") {
+        console.log(config);
+    } else {
+        console.log(config.path, config.state);
+    }
+}
+
+type Custom = {
+    (a: number, b: number) : number
+    (a: number, b: number, c: number) : number
+}
+
+// c 는 optional
+const custom:Custom = (a, b, c?: number) => {
+
+    if(c) return a + b + c;
+
+    return a + b;
+}
+
+custom(1, 2);
+custom(1, 2, 3);
