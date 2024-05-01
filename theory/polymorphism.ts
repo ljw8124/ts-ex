c08e17efa0e5b742c9500e80108390a515e33b6c
Joungwoo Lee <ljw8124@gmail.com>
2024-05-01 17:44:00 +0900

    generic

--- theory/polymorphism.ts
+++ theory/polymorphism.ts
@@ -7,7 +7,7 @@ type SuperPrint = {
 
     // generic -> placeholder 와 같음. Type 을 유추하도록 함
     // any 와 다르게 타입에 대한 정보를 가지고 있게 되어 유리함
-    <T>(arr: T[]) :T
+    <T, M>(arr: T[], b?: M) :T
 }
 
 const superPrint: SuperPrint = (arr) => {
@@ -16,7 +16,7 @@ const superPrint: SuperPrint = (arr) => {
     return arr[0];
 }
 
-const a = superPrint([1, 2, 3, 4]);
-const b = superPrint([true, false, true, true]);
-const c = superPrint(['a', 'b', 'c']);
-const d = superPrint([1, 2, true, false]);
+const a = superPrint([1, 2, 3, 4], "X");
+const b = superPrint([true, false, true, true], 1);
+const c = superPrint(['a', 'b', 'c'], false);
+const d = superPrint([1, 2, true, false], []);
