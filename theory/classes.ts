9562b0ad844e5a0ca1e01fa4e79a8ca20e3d787c
Joungwoo Lee <ljw8124@gmail.com>
2024-05-04 15:38:51 +0900

    부연설명 주석추가

--- theory/classes.ts
+++ theory/classes.ts
@@ -16,14 +16,16 @@ const joungwoo = new Player("lee", 'joung', '정우');
 // 추상 클래스
 // 추상 클래스는 다른 클래스가 상속받을 수 있도록 만드는 클래스이고,
 // 직접 인스턴스를 만들 수는 없다
+// 물론 JS 로 컴파일되면서 아래 범위 같은 부분은 지워짐
 abstract class User {
     constructor(
         private firstName: string,
         private lastName: string,
-        protected nickName: string
+        protected nickName: string  // 자바에서와 같이 protected 로 해당 변수 접근성을 줄 수 있음
     ) {
     }
 
+    // 추상메서드를 선언함으로써, 상속받는 자식 클래스에서 구현해야함
     abstract getNickName():void
 
     getFullName() {
@@ -32,6 +34,7 @@ abstract class User {
 }
 
 class Tutor extends User{
+    // 추상메서드가 있으므로 오버라이딩 해야함(필수)
     getNickName() {
         console.log(this.nickName);
     }
