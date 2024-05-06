299fb11210037b241f2117a3a5afb136393f974d
Joungwoo Lee <ljw8124@gmail.com>
2024-05-06 19:03:19 +0900

    directory 이름 변경/dictionary.ts 추가

---
+++ nomadcoders_src/dictionary.ts
@@ -0,0 +1,47 @@
+// 아래 타입 구조는 string 만을 property 로 가지는 Object 라는 것을 명시해줌
+type Words = {
+    [key: string]: string;
+}
+
+class Dict {
+    private words: Words;
+
+    constructor() {
+        this.words = {};
+    }
+
+    add(word: Word): void {
+        if(this.words[word.term] === undefined) {
+            this.words[word.term] = word.def;
+        }
+    }
+
+    getWord(term: string): string {
+
+        let returnValue: string;
+
+        if(!this.words[term]) {
+            returnValue = '검색 결과 없음';
+        } else {
+            returnValue = this.words[term];
+        }
+
+        return returnValue;
+    }
+}
+
+class Word {
+    constructor(
+        public term: string,
+        public def: string
+    ) {
+    }
+}
+
+const kimchi = new Word("Kimchi", "한국의 음식");
+
+const dict = new Dict();
+
+dict.add(kimchi);
+
+dict.getWord("kimchi");
