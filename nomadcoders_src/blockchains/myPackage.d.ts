67df323ec5249cd94e0cfa1237e768d37a8c68a0
Joungwoo Lee <ljw8124@gmail.com>
2024-05-25 12:17:34 +0900

    allow js, JSDoc 사용

--- nomadcoders_src/blockchains/myPackage.d.ts
+++ nomadcoders_src/blockchains/myPackage.d.ts
@@ -4,7 +4,7 @@ interface Config {
     url: string;
 }
 
-declare module "myPackage" {
+declare module myPackage {
     function init(config: Config): boolean;
 
     function exit(code: number): number;
--- nomadcoders_src/blockchains/myPackage.js
+++ nomadcoders_src/blockchains/myPackage.js
@@ -1,7 +1,23 @@
+// JS 파일임에도 불구하고 타입스크립트 체크가 필요한 경우
+// 아래 주석을 통해서 JS 처럼 타입스크립트처럼 인식함
+
+// @ts-check
+/**
+ * Initializes the project
+ * @param {object} config
+ * @param {boolean} config,debug
+ * @param {string} config.url
+ * @returns {boolean}
+ */
 export function init(config) {
     return true;
 }
 
+/**
+ * exit the project
+ * @param {number} code
+ * @returns {number}
+ */
 export function exit(code) {
     return code + 1;
 }
