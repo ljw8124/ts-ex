9a42db71de87dcb09431b0f547fd07324d466cc0
LeeJoungWoo <ljw8124@gmail.com>
2025-02-12 15:59:18 +0900

    add block code

--- nomadcoders_src/blockchains/index.ts
+++ nomadcoders_src/blockchains/index.ts
@@ -1,7 +1,36 @@
-import {init, exit} from "./myPackage";
+// import {init, exit} from "./myPackage";
 
-init({
-    url: "true"
-});
+// init({
+//     url: "true"
+// });
 
-exit(1);
+// exit(1);
+
+// console.log('hi');
+
+import crypto from "crypto";
+
+interface BlockShape {
+    hash: string;
+    prevHash: string;
+    height: number;
+    data: string;
+}
+
+class Block implements BlockShape {
+    public hash: string;
+    constructor(
+        public prevHash: string,
+        public height: number,
+        public data: string,
+    ) {
+        this.hash = Block.calculateHash(prevHash, height, data);
+    }
+
+    static calculateHash(prevHash: string, height: number, data: string): string {
+        const toHash: string = `${prevHash}${height}${data}`;
+        const salt = "whatever";
+
+        return crypto.hash(toHash, salt);
+    }
+}
