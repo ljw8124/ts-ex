834d9c89ed3e23faba20ca138b5dfb32a6c5ae4a
Joungwoo Lee <ljw8124@gmail.com>
2025-03-13 14:01:11 +0900

    이진탐색구현

--- effective-typescript/binarySearch.ts
+++ effective-typescript/binarySearch.ts
@@ -1,1 +1,26 @@
+// 이진탐색구현
+function binarySearch(arr: Array<number>, target: number) {
+    let idxHigh: number = arr.length - 1;
+    let idxLow: number = 0;
+    let returnVal: number = -1;
 
+    while(idxLow <= idxHigh) {
+        let idxMid: number = Math.floor((idxHigh + idxLow) / 2);
+
+        if(arr[idxMid] == target) {
+            returnVal = idxMid;
+            break;
+        }
+
+        if(arr[idxMid] < target) {
+            idxLow = idxMid + 1;
+        } else if(arr[idxMid] > target) {
+            idxHigh = idxMid - 1;
+        }
+    }
+
+    // 실패할 경우
+    return `${returnVal} 번째 파라미터`
+}
+
+console.log(binarySearch([1,2,3,4,5], 3));
